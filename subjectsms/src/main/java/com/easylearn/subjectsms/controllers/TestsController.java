20c1e4a33eaff95e50f57dcf6975b62ace0e8fa0
Andrej Suba <103282953+AndrejSub@users.noreply.github.com>
2024-04-02 11:15:04 +0200

    #0008 Removing errors

--- subjectsms/src/main/java/com/easylearn/subjectsms/controllers/TestsController.java
+++ subjectsms/src/main/java/com/easylearn/subjectsms/controllers/TestsController.java
@@ -2,9 +2,7 @@ package com.easylearn.subjectsms.controllers;
 
 import com.easylearn.subjectsms.collection.Tests;
 import com.easylearn.subjectsms.controllers.BaseController.BaseController;
-import com.easylearn.subjectsms.dtos.SubjectsDto;
 import com.easylearn.subjectsms.dtos.TestsDto;
-import com.easylearn.subjectsms.services.Subjects.SubjectsService;
 import com.easylearn.subjectsms.services.TestServices.ITestService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
