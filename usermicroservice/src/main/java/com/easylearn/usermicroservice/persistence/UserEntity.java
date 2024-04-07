fe8f76b25fb81a32edd8963df68a327bd00809f3
mprykhodko <mprykhodko@student.umb.sk>
2024-04-07 13:44:41 +0200

    Security layer. Version with bugs

--- usermicroservice/src/main/java/com/easylearn/usermicroservice/persistence/UserEntity.java
+++ usermicroservice/src/main/java/com/easylearn/usermicroservice/persistence/UserEntity.java
@@ -4,19 +4,26 @@ package com.easylearn.usermicroservice.persistence;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Document(collection = "info")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Transient
     public static final String SEQUENCE_NAME = "users_sequence";
     @Id
@@ -34,4 +41,30 @@ public class UserEntity {
     @Field
     private Date updated_at;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        GrantedAuthority - list of roles
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
