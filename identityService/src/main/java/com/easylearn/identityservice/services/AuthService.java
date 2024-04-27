b00d72e393875f38d80746befd111ed2b53789c9
Andrej Suba <103282953+AndrejSub@users.noreply.github.com>
2024-04-27 16:19:13 +0200

    Password change

--- identityService/src/main/java/com/easylearn/identityservice/services/AuthService.java
+++ identityService/src/main/java/com/easylearn/identityservice/services/AuthService.java
@@ -38,6 +38,26 @@ public class AuthService {
         }
     }
 
+    public ResponseEntity changePassword(UserInfo info){
+        try {
+            UserInfo user = this.getUserInfoByEmail(info.getEmail());
+            if(user.getEmail() == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            user.setPassword(passwordEncoder.encode(info.getPassword()));
+            userInfoRepository.save(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception exception){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+
+    public UserInfo getUserInfoByEmail(String email){
+        return userInfoRepository.findByEmail(email).orElse(null);
+    }
+
     public String generateToken(String username){
         return jwtService.generateToken(username);
     }
