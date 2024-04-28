b92f1a2ffeef741d73f9fe93f6cc2462682a7ce6
mprykhodko <mprykhodko@student.umb.sk>
2024-04-28 20:03:49 +0200

    be part of avatar

--- usermicroservice/src/main/java/com/easylearn/usermicroservice/controllers/UserController.java
+++ usermicroservice/src/main/java/com/easylearn/usermicroservice/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.easylearn.usermicroservice.dtos.UserDTO;
 import com.easylearn.usermicroservice.persistence.UserEntity;
 import com.easylearn.usermicroservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,22 +14,22 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService userService;
-    @GetMapping("/{id}")
-    public UserDTO getUserById(@PathVariable String id){
-        return userService.getUser(id);
+    @GetMapping("/{email}")
+    public UserDTO getUserByEmail(@PathVariable String email){
+        return userService.getUserByEmail(email);
     }
-    @PostMapping()
-    public UserEntity createUser(@RequestBody UserDTO userDTO){
-        return userService.createUser(userDTO);
-    }
+//    @PostMapping()
+//    public UserEntity createUser(@RequestBody UserDTO userDTO){
+//        return userService.createUser(userDTO);
+//    }
     @GetMapping()
     public List<UserDTO> getAllUsers(){
         return userService.getAllUsers();
     }
-    @PutMapping("/{id}")
-    public UserEntity updateUser(@PathVariable String id, @RequestBody UserDTO userDTO){
-        return userService.updateUser(id, userDTO);
-    }
+//    @PutMapping("/{id}")
+//    public UserEntity updateUser(@PathVariable String id, @RequestBody UserDTO userDTO){
+//        return userService.updateUser(id, userDTO);
+//    }
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable String id){
         userService.deleteUser(id);
@@ -37,5 +38,13 @@ public class UserController {
     public void deleteUserbyEmail(@PathVariable String email){
         userService.deleteUserbyEmail(email);
     }
-
+//    @PatchMapping("/{email}")
+//    public void updateUserPhoto(@PathVariable String email, @RequestBody UserDTO userDTO){
+//        System.out.println("zmenil som");
+//        userService.updateUserPhoto(email, userDTO);
+//    }
+    @PostMapping("/update")
+    public Long changePhoto(@RequestBody UserEntity userEntity){
+        return this.userService.upadateUserByPost(userEntity);
+    }
 }
